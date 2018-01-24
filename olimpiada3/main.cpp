19fc233c189edbb4d94c7bc79f0779a07dcde690
Zaharia Tudorita <ztudorita@gmail.com>
2018-01-24 17:50:40 +0200

    ultra sexy doge did it, ultra super anal

--- olimpiada3/main.cpp
+++ olimpiada3/main.cpp
@@ -4,44 +4,79 @@
 using namespace std;
 ifstream fin("date.in");
 ofstream fout("date.out");
-int compare(char a[],char b[])
+void sfincter(char a[],char b[],unsigned i,unsigned &j,int &ok)
 {
-    int i = 0;
-    int j = 0;
-    while(i < strlen(b) && j < strlen(a))
+    if(a[i] != b[j] && j < strlen(b))
     {
-        if(b[i] == a[j])
-            {
-                i++;
-                j++;
-            }
-            else if(b[i] != a[j])
-            {
-                if(a[j] == '*')
-                {
-                    if(a[j+1] == '?')
-                    {
-                        j= j+2;
-                        while(a[j] != b[i] && i <= strlen(b)) i++;
-                        if(strlen(b) == i) return 0;
-                    }
-                    else
-                    {
-                        j++;
-                        while(a[j] != b[i] && i <= strlen(b)) i++;
-                        if(strlen(b) == i) return 0;
-                    }
-
-                }
-                else if(a[j] == '?')
-                {
-                    j++;
-                    i++;
-                }
-            }
-            else return 0;
+        j++;
+        sfincter(a,b,i,j,ok);
     }
-    return 1;
+    else
+    {
+        if(j == strlen(b)) ok = 1;
+        if(a[i] == b[j]) ok = 2;
+    }
+
+}
+void marakesh(char a[],char b[],int &pas)
+{
+    if(a[strlen(a)-pas] == b[strlen(b)-pas])
+    {
+        pas++;
+        marakesh(a,b,pas);
+    }
+}
+void barbat(char a[],char b[],unsigned &i,unsigned &j)
+{
+    if(a[i] == '*' || a[i] == '?')
+    {
+        if(a[i] == '?') j++;
+        i++;
+        barbat(a,b,i,j);
+    }
+}
+void compare(char a[],char b[],unsigned i,unsigned j,int pas,int &bine)
+{
+    if(i == strlen(a)-1 && a[i] == '*' && bine == 1)
+    {
+        bine = 2;
+        compare(a,b,i,j,pas,bine);
+    }
+    else
+    if(a[i] == b[j] && bine == 1)
+    {
+         compare(a,b,i+1,j+1,pas,bine);
+    }
+    else if(a[i] == '*' && bine == 1)
+    {
+        int ok = 0;
+        if(a[i+1] != '*' && a[i+1] != '?')
+        {
+            sfincter(a,b,i,j,ok);
+        }
+        else barbat(a,b,i,j);
+        {
+            sfincter(a,b,i,j,ok);
+        }
+        if(ok == 2)
+        {
+            compare(a,b,i,j,pas,bine);
+        }
+        else if(ok == 1)
+        {
+            bine = 0;
+            compare(a,b,i,j,pas,bine);
+        }
+    }
+    else if(a[i] == '?' && bine == 1)
+    {
+        return compare(a,b,i+1,j+1,pas,bine);
+    }
+    else if(a[i] != b[j] && (a[i] != '*' && a[i]!='?') && bine == 1 && j < strlen(b))
+                {
+                    bine = 0;
+                    compare(a,b,i,j,pas,bine);
+                }
 }
 int main()
 {
@@ -49,6 +84,11 @@ int main()
     char b[50];
     fin >> a;
     while(fin >> b)
-        fout << compare(a,b);
+    {
+        int bine = 1;
+        compare(a,b,0,0,0,bine);
+        fout << bine << endl;
+    }
+
     return 0;
 }
